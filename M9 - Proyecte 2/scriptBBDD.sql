db705d5f026d0342628a6d7ee13b174ed734d6ff
C734R <alan_g_s93@hotmail.com>
2025-01-23 12:37:52 +0100

    - Corregido user admin

--- M9 - Proyecte 2/scriptBBDD.sql
+++ M9 - Proyecte 2/scriptBBDD.sql
@@ -32,7 +32,7 @@ CREATE TABLE alquileres (
 );
 
 INSERT INTO usuarios(usuario,password,nombre,apellido1,apellido2)
-    VALUES ('admin', '1234' , 'super', 'mega', 'admin');
+    VALUES ('admin', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4' , 'super', 'mega', 'admin');
 
 INSERT INTO coches(modelo,cantidad,precio)
     VALUES ('HONDAS2000', 3 , 250.0);
